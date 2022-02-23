434cdcec12353a657c874ce961ed7555dbeeeb3d
Haseeb Hasan <hh508@ilab1.cs.rutgers.edu>
2022-02-23 09:22:01 -0500

    Implement client and root server communication

---
+++ project2/client.py
@@ -0,0 +1,34 @@
+import time
+import socket
+
+try:
+    cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("[C]: Client socket created")
+except socket.error as err:
+    print('socket open error: {} \n'.format(err))
+    exit()
+    
+# Define the port on which you want to connect to the server
+port = 50007
+localhost_addr = socket.gethostbyname(socket.gethostname())
+
+# connect to the server on local machine
+server_binding = (localhost_addr, port)
+cs.connect(server_binding)
+
+# Reading from file and sending to server
+lines = []
+with open('PROJ2-HNS.txt', 'r') as inf:
+    lines = inf.readlines()
+inf.close()
+outf = open("RESOLVED.txt", "w")
+for line in lines:
+    cs.sendall(line.encode('utf-8'))
+    time.sleep(1)
+    msg = cs.recv(200)
+    msg = msg.decode('utf-8') 
+    outf.write(msg)
+
+# close the client socket
+cs.close()
+exit()
---
+++ project2/rs.py
@@ -0,0 +1,27 @@
+import socket
+
+try:
+    ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("[S]: Server socket created")
+except socket.error as err:
+    print('socket open error: {}\n'.format(err))
+    exit()
+
+server_binding = ('', 50007)
+ss.bind(server_binding)
+ss.listen(1)
+host = socket.gethostname()
+print("[S]: Server host name is {}".format(host))
+localhost_ip = (socket.gethostbyname(host))
+print("[S]: Server IP address is {}".format(localhost_ip))
+csockid, addr = ss.accept()
+print ("[S]: Got a connection request from a client at {}".format(addr))
+
+# receive message from the client.
+while True:
+    msg = csockid.recv(200)
+    if len(msg) == 0:
+        # Close the server socket
+        ss.close()
+        exit()
+    csockid.sendall(msg.encode('utf-8'))
